6770ae6cc8326388c66c042d098f25c2c1f1f4d3
paradingwolves <justinbri35@gmail.com>
2022-11-12 11:40:54 -0500

    start of slide 16

---
+++ JustinsBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using JustinsBooks.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustinsBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //API Calls Here
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { date = allObj });
+        }
+    }
+}
